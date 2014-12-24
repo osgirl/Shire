e4f4ca4031ae3186c1098004f94cca584d984560
Raymond Wu <raywu22@mit.edu>
2014-12-24 02:51:47 -0500

    events are done!! yay

--- public/javascripts/eventsScript.js
+++ public/javascripts/eventsScript.js
@@ -1,23 +1,24 @@
 $(document).ready(function(){
-	events = [{
-		"title": "Christmas Celebration",
-		"description": "This was made using javascript ;D",
-		"event-where": "my house",
-		"event-when": "christmas day",
-		"meet-where": "your house",
-		"meet-when": "christmas eve"
-	},
-	{
-		"title": "Christmas Celebration2",
-		"description": "This was made using javascript ;D",
-		"event-where": "my house",
-		"event-when": "christmas day",
-		"meet-where": "your house",
-		"meet-when": "christmas eve"
-	}];
-	for (var i in events){
-		makeEvent(events[i]);
-	}
+	// events = [{
+	// 	"title": "Christmas Celebration",
+	// 	"description": "This was made using javascript ;D",
+	// 	"eventWhere": "my house",
+	// 	"eventWhen": "christmas day",
+	// 	"meetWhere": "your house",
+	// 	"meetWhen": "christmas eve"
+	// },
+	// {
+	// 	"title": "Christmas Celebration2",
+	// 	"description": "This was made using javascript ;D",
+	// 	"eventWhere": "my house",
+	// 	"eventWhen": "christmas day",
+	// 	"meetWhere": "your house",
+	// 	"meetWhen": "christmas eve"
+	// }];
+	// for (var i in events){
+	// 	makeEvent(events[i]);
+	// }
+
 });
 
 function makeEvent(event){
@@ -27,12 +28,12 @@ function makeEvent(event){
 							"<input class='mySubmit' id='event-delete' type='submit' value='Delete'/>"+
 							"<div class='event-table-outer'>"+
 							"<div class='event-info'>"+
-							"<p class='event-where'>Where: "+event["event-where"]+"</p>"+
-							"<p class='event-when'>When: "+event["event-when"]+"</p>"+
+							"<p class='eventWhere'>Where: "+event["eventWhere"]+"</p>"+
+							"<p class='eventWhen'>When: "+event["eventWhen"]+"</p>"+
 							"</div>"+
 							"<div class='meet-info'>"+
-							"<p class='meet-where'>Meeting where: "+event["meet-where"]+"</p>"+
-							"<p class='meet-when'>Meeting when: "+event["meet-when"]+"</p>"+
+							"<p class='meetWhere'>Meeting where: "+event["meetWhere"]+"</p>"+
+							"<p class='meetWhen'>Meeting when: "+event["meetWhen"]+"</p>"+
 							"</div>"+
 							"</div>"+
 							"</div>");
--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
-/* GET home page. */
+var Schema = mongoose.Schema;
+var eventsSchema = new Schema({
+	title: String,
+	description: String,
+	eventWhere: String,
+	eventWhen: String,
+	meetWhere: String,
+	meetWhen: String
+})
+var eventsModel = mongoose.model("events",eventsSchema);
+
 router.get('/', function(req, res) {
-  res.render('events');
+	eventsModel.find(function(err,allEvents){
+
+		res.render('events',{events:allEvents});
+	})
 });
+
+router.post('/add_event',function(req,res){
+	var newEvent = new eventsModel({
+		title: req.body.title,
+		description: req.body.description,
+		eventWhere: req.body.eventWhere,
+		eventWhen: req.body.eventWhen,
+		meetWhere: req.body.meetWhere,
+		meetWhen: req.body.meetWhen
+	});
+	newEvent.save(function(error){
+		if (error){
+			return handleError(error);
+		}
+	})
+	res.redirect('/events');
+})
+
+router.post('/deleteEvent',function(req,res){
+	eventsModel.findOneAndRemove({eventID:req.body._id},function(){
+	});
+	res.redirect('/events');
+})
 
 module.exports = router;
