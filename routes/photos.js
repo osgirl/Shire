bd30b8502d50c9c45523da1ac83e99d14e11e9ef
Raymond Wu <raywu22@mit.edu>
2014-12-31 04:48:40 -0500

    adding from file works now!! =D

--- routes/photos.js
+++ routes/photos.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var formidable = require('formidable');
+var fs = require('fs');
+var path = require('path');
 
 var Schema = mongoose.Schema;
 var photoAddUrlSchema = new Schema({
@@ -40,9 +43,38 @@ router.post('/urlAdd',function(req,res){
 	}
 })
 
-router.post('/fileAdd',function(req,res){
-	console.log(req.body);
-})
+
+router.post('/fileAdd', function(req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        // `file` is the name of the <input> field of type `file`
+        var old_path = files.toAddFile.path;
+        var file_size = files.toAddFile.size;
+        var file_name = files.toAddFile.name.split('/').pop();
+        var directoryName = __dirname.substring(0,__dirname.length-7);
+        var new_path = path.join(directoryName, 'public/images/shire_photos/', file_name);
+        fs.readFile(old_path, function(err, data) {
+            fs.writeFile(new_path, data, function(err) {
+                fs.unlink(old_path, function(err) {
+                    if (err) {
+                        res.send("unsuccessful");
+                    } else {
+                    	var newPhotoUrl = new photoAddUrlModel({
+                    		photoUrl: "images/shire_photos/"+file_name
+                    	})
+                    	newPhotoUrl.save(function(error){
+                    		if (error){
+                    			return handleError(error)
+                    		}
+                    		res.redirect('/photos');
+                    	})
+                    }
+                });
+            });
+        });
+    });
+});
+
 
 router.post('/deletePhoto',function(req,res){
 	if (req.session.currentUser!=="rayray"){
@@ -53,7 +85,6 @@ router.post('/deletePhoto',function(req,res){
 			res.send("deleteSuccessful");
 		});
 	}
-
 })
 
 module.exports = router;
