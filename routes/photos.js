f372891b9512b5e0f92e3bb8bd5e481addd1ddc0
Raymond Wu <raywu22@mit.edu>
2014-12-31 22:28:59 -0500

    changed backend permissions

--- routes/photos.js
+++ routes/photos.js
@@ -13,66 +13,79 @@ var photoAddUrlModel = mongoose.model("photoUrl",photoAddUrlSchema);
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	//req.session.currentUser=true;
 	if (!req.session.currentUser){
-		res.render('login',{message:"please log in",
-							isLoggedIn:false});
-	}
-	else {
 		photoAddUrlModel.find(function(error,allPhotoUrls){
 			res.render('photos',{allUrlPhotos:allPhotoUrls,
-								isLoggedIn:true});
+								isLoggedIn:false});
 		});
 	}
+	else {
+	photoAddUrlModel.find(function(error,allPhotoUrls){
+		res.render('photos',{allUrlPhotos:allPhotoUrls,
+							isLoggedIn:true});
+	});
+	}
+
 });
 
 router.post('/urlAdd',function(req,res){
-	if (req.body.toAddUrl.substring(0,4)==="http"){
-		var newPhotoUrl = new photoAddUrlModel({
-			photoUrl: req.body.toAddUrl.toString()
-		})
-		newPhotoUrl.save(function(error){
-			if (error){
-				return handleError(error);
-			}
-			res.redirect('/photos');
-		});
+	if (!req.session.currentUser){
+		res.send('Login To Add');
 	}
 	else {
-		res.redirect('/photos');
+		if (req.body.toAddUrl.substring(0,4)==="http"){
+			var newPhotoUrl = new photoAddUrlModel({
+				photoUrl: req.body.toAddUrl.toString()
+			})
+			newPhotoUrl.save(function(error){
+				if (error){
+					return handleError(error);
+				}
+				res.send("addSuccessful");
+			});
+		}
+		else {
+			res.send("Please enter valid url");
+		}		
 	}
 })
 
 
 router.post('/fileAdd', function(req, res) {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files) {
-        // `file` is the name of the <input> field of type `file`
-        var old_path = files.toAddFile.path;
-        var file_size = files.toAddFile.size;
-        var file_name = files.toAddFile.name.split('/').pop();
-        var directoryName = __dirname.substring(0,__dirname.length-7);
-        var new_path = path.join(directoryName, 'public/images/shire_photos/', file_name);
-        fs.readFile(old_path, function(err, data) {
-            fs.writeFile(new_path, data, function(err) {
-                fs.unlink(old_path, function(err) {
-                    if (err) {
-                        res.render("index",{message:"Upload Unsuccessful"});
-                    } else {
-                    	var newPhotoUrl = new photoAddUrlModel({
-                    		photoUrl: "images/shire_photos/"+file_name
-                    	})
-                    	newPhotoUrl.save(function(error){
-                    		if (error){
-                    			return handleError(error)
-                    		}
-                    		res.redirect('/photos');
-                    	})
-                    }
-                });
-            });
-        });
-    });
+	if (!req.session.currentUser){
+		res.send('Login To Add');
+	}
+	else {
+	    var form = new formidable.IncomingForm();
+	    form.parse(req, function(err, fields, files) {
+	        // `file` is the name of the <input> field of type `file`
+	        var old_path = files.toAddFile.path;
+	        var file_size = files.toAddFile.size;
+	        var file_name = files.toAddFile.name.split('/').pop();
+	        var directoryName = __dirname.substring(0,__dirname.length-7);
+	        var new_path = path.join(directoryName, 'public/images/shire_photos/', file_name);
+	        fs.readFile(old_path, function(err, data) {
+	            fs.writeFile(new_path, data, function(err) {
+	                fs.unlink(old_path, function(err) {
+	                    if (err) {
+	                        res.render("index",{message:"Upload Unsuccessful"});
+	                    } else {
+	                    	var newPhotoUrl = new photoAddUrlModel({
+	                    		photoUrl: "images/shire_photos/"+file_name
+	                    	})
+	                    	newPhotoUrl.save(function(error){
+	                    		if (error){
+	                    			return handleError(error)
+	                    		}
+	                    		res.send("addSuccessful");
+	                    	})
+	                    }
+	                });
+	            });
+	        });
+	    });		
+	}
+
 });
 
 
