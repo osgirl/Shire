0bd01e0a91b0effdd79144e913760f1ed8af9abb
Raymond Wu <raywu22@mit.edu>
2015-01-02 05:00:26 -0500

    tried fixing thin navbar

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var db = mongoose.connection;
 db.on('error',console.error.bind(console,'connection error:'));
 db.once('open',function(callback){
 	console.log("Connected to database!");
-})
+});
 
 var routes = require('./routes/index');
 var history = require('./routes/history');
